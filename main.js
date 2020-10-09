1db5137ff0184a21a34b8f7c6ad0d59bacaeaa9e
Jessica Salbert <jessicarsalbert@gmail.com>
2020-10-09 13:35:24 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -8,22 +8,35 @@ document.addEventListener('DOMContentLoaded', () => {
   document.body.addEventListener('click', function(e) {
     if (e.target.matches('.like-glyph')) {
       const resp = mimicServerCall()
+<<<<<<< HEAD
       .then( function() { 
         e.target.classList.add("activated-heart")
         e.target.textContent = FULL_HEART
       })
+=======
+      .then( () => e.target.textContent = FULL_HEART)
+>>>>>>> b79759a892660f2d1b2c05461855d55c6a0f1d0f
       .catch( function(error) {
         const errorModal = document.getElementById('modal')
         errorModal.textContent = error
         errorModal.classList.remove("hidden")
         setTimeout( () => errorModal.classList.add("hidden"), 5000 )
       })
+<<<<<<< HEAD
+=======
+      if (resp === "") {
+        e.target.textContent = FULL_HEART
+      }
+>>>>>>> b79759a892660f2d1b2c05461855d55c6a0f1d0f
 
     }
   })
   
 })
+<<<<<<< HEAD
 
+=======
+>>>>>>> b79759a892660f2d1b2c05461855d55c6a0f1d0f
 
 
 
